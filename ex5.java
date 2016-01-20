9b5fd0bee3be68fec11f65380243d6a8abcd5347
ArgentumSunset <coltergiem@gmail.com>
2016-01-19 22:08:42 -0700

    Exercise 5 Complete

--- ex5.java
+++ ex5.java
@@ -1,4 +1,5 @@
 import java.io.* ;
+import java.util.* ;
 
 class ReverserVersion1
 {
@@ -6,20 +7,19 @@ class ReverserVersion1
   public static void main ( String[] args ) 
   {
     int[] data = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+    List<Integer> reversed = new ArrayList<Integer>();
     
     // reverse the data
-    for ( int j=0; j < data.length / 2; j++)
+    for ( int j=0; j < data.length; j++)
     {
-      int i = data[j];
-      data[j] = data[data.length - 1 - j];
-      data[data.length - 1 - j] = i;
+      reversed.add(data[data.length - 1 - j]);
     }
       
     // write out the new data
     for ( int j=0; j < data.length; j++)
     {
-      System.out.println(data[j]);
+      System.out.println(reversed.get(j));
     }
 
   }
-}          
+}  
